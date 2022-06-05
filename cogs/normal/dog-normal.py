2d3185cbff5a488fcf1c8c5b34a778067e49993d
James McDowell <wejrox@users.noreply.github.com>
2022-06-05 12:54:26 +1000

    Refactored concerns into an OOP, with DogAct owning its own mutations. Added more documentation.

--- cogs/normal/dog-normal.py
+++ cogs/normal/dog-normal.py
@@ -1,4 +1,5 @@
 # Here we name the cog and create a new class for the cog.
+from typing import Union
 
 import disnake
 from disnake import Member
@@ -8,70 +9,113 @@ from disnake.ext.commands import Context
 
 class DogAct:
     """
-    Represents an instance in which users have voted whether a specific act should be considered dog.
+    Represents an instance in which users cast votes to determine whether a specific act should be considered dog.
 
-    Attributes
-    ----------
-    message_id: :class:`int`
-        Unique identifier for the bot message that this relates to.
-    reporter: :class:`Member`
-        Who it was that filed this report.
-    target: :class:`Member`
-        Who it is that allegedly dogged.
-    allegation: :class:`str`
-        Reason that the reporter said they are guilty.
-    yes_votes: :class:`list[Member]`
-        Unique member identifiers for those that voted yes.
-    no_votes: :class:`Member`
-        Unique member identifiers for those that voted no.
-    required_votes: :class:`int`
-        How many votes are required in order for this DogAct to be finalised in either direction.
+    :ivar yes_votes: Unique member identifiers for those that voted yes.
+    :ivar no_votes: Unique member identifiers for those that voted no.
+    :ivar timed_out: Whether this dog act sat idle for too long and has been cancelled.
     """
-
-    # Unique identifier for the bot message that this relates to.
-    message_id: int
-
-    # Who it was that reported this person.
-    reporter: Member
-
-    # Who it is that allegedly dogged.
-    target: Member
-
-    # Reason that the reporter said they are guilty.
-    allegation: str
-
-    # Unique member identifiers for those that voted yes.
-    yes_votes: list[Member] = []
-
-    # Unique member identifiers for those that voted no.
-    no_votes: list[Member] = []
-
-    # How many votes are required in order for this DogAct to be finalised in either direction.
-    required_votes: int
 
     def __init__(self, reporter: Member, target: Member, allegation: str,
                  message_id: int = -1, required_votes: int = 1):
+        """
+        :param reporter: Who it was that filed this report against the target.
+        :param target: The user accused of being a dog.
+        :param allegation: The reason provided by the reporter as to why they should be considered a dog.
+        :param message_id: Unique identifier for the bot message that this relates to.
+        Can't exist before a message has been sent by the bot to the server.
+        :param required_votes: How many votes are required in order for this DogAct to be finalised in either direction.
+        """
+        self.message_id = message_id
         self.reporter = reporter
         self.target = target
         self.allegation = allegation
-        self.message_id = message_id
         self.required_votes = required_votes
 
-    def vote_outcome(self):
-        if len(self.no_votes) >= self.required_votes:
+        # Default values for mutable variables.
+        self.yes_votes: list[Member] = []
+        self.no_votes: list[Member] = []
+        self.timed_out = False
+
+    def vote_outcome(self) -> Union[bool, None]:
+        """
+        Determines whether the voting has reached a definitive outcome of Guilty or Not Guilty.
+
+        :returns: False when Not Guilty or timed out, True when Guilty, None when not enough votes have been cast.
+        """
+        if len(self.no_votes) >= self.required_votes or self.timed_out:
             return False
         elif len(self.yes_votes) >= self.required_votes:
             return True
         else:
             return None
 
-    def create_updated_dog_act_message(self):
+    def add_new_yes_vote(self, author: Member) -> None:
+        """
+        Updates the vote for the provided author to be a 'yes' vote.
+
+        :param author: Discord Member to be added to the 'yes' list.
+        """
+        self._clear_votes_for_author(author)
+        self.yes_votes.append(author)
+
+    def add_new_no_vote(self, author: Member) -> None:
+        """
+        Updates the vote for the provided author to be a 'no' vote.
+
+        :param author: Discord Member to be added to the 'no' list.
+        """
+        self._clear_votes_for_author(author)
+        self.no_votes.append(author)
+
+    def create_updated_dog_act_message(self) -> str:
         """
         Generates a message detailing information about this dog acct report, including the current status of voting.
+
+        :returns: An up-to-date representation of the status of this dog act.
         """
         return (f"Whoah there {self.reporter.mention}, that's a big claim!\n"
                 f"Who agrees that {self.target.mention} was really a dog for '{self.allegation}'?\n"
                 f"Current votes: Guilty - {len(self.yes_votes)}, Not Guilty - {len(self.no_votes)}")
+
+    def create_outcome_message(self) -> str:
+        """
+        Generates a message indicating to the reader what the outcome was of the dog act trial.
+
+        :return: Whether the target is guilty or innocent.
+        """
+        if self.vote_outcome():
+            return f"{self.target.mention} has been found guilty of being a dog for '{self.allegation}'!"
+        else:
+            return f"{self.target.mention} is innocent! Shame on {self.reporter.mention}"
+
+    def create_detailed_outcome_message(self) -> str:
+        """
+        Creates a detailed outcome message containing information about the trial.
+        Shouldn't be sent to the server as it's not in a nice to read format.
+
+        :returns: Information about the dog act.
+        """
+
+        def get_voter_name(user: Member): return user.name
+
+        guilty_voters: list[str] = list(map(get_voter_name, self.yes_votes))
+        not_guilty_voters: list[str] = list(map(get_voter_name, self.no_votes))
+        return (f"Dog act finalised. "
+                f"Verdict: {self.create_outcome_message()}. "
+                f"Guilty voters: {guilty_voters}, Not guilty voters: {not_guilty_voters} "
+                f"Trial timed out? {self.timed_out}")
+
+    def _clear_votes_for_author(self, author: Member) -> None:
+        """
+        Removes all votes previously made by the provided author from any relevant lists.
+
+        :param author: Discord Member to be removed.
+        """
+        if author in self.yes_votes:
+            self.yes_votes.remove(author)
+        if author in self.no_votes:
+            self.no_votes.remove(author)
 
 
 class DogChoice(disnake.ui.View):
@@ -84,34 +128,47 @@ class DogChoice(disnake.ui.View):
     Once the required number of votes for a particular option have been cast, a decision is reached and the choices end.
     If there aren't enough choices by the duration of _timeout_sec then the target is presumed innocent.
     """
-    _timeout_sec: int = 5 * 60
 
-    def __init__(self, related_dog_act: DogAct):
-        super().__init__(timeout=self._timeout_sec)
+    def __init__(self, related_dog_act: DogAct, timeout_sec: int = 5 * 60):
+        """
+
+        :param related_dog_act: Dog act that is being voted on by this Dog choice.
+        :param timeout_sec: Seconds to wait before this view times out and is no longer valid.
+        """
+        super().__init__()
         self.dog_act = related_dog_act
+        self.timeout = timeout_sec
 
     @disnake.ui.button(label="Definitely a dog", style=disnake.ButtonStyle.blurple)
-    async def yes(self, _: disnake.ui.Button, interaction: disnake.MessageInteraction):
-        self.clear_votes_for_author(interaction.author)
-        self.dog_act.yes_votes.append(interaction.author)
+    async def yes(self, _: disnake.ui.Button, interaction: disnake.MessageInteraction) -> None:
+        """
+        Generates the 'yes' option. Clicking this button causes the clicker to be added to the 'yes' votes list.
+        To avoid adding the same user multiple times, their votes are cleared before adding them to the correct list.
+        Once the vote has been counted, the view is exited, and the message can be updated as required.
 
-        await self.finish_if_complete()
+        :param _: Unused parameter for the button the user can click.
+        :param interaction: Details about the interaction that occurred with the button.
+        """
+        self.dog_act.add_new_yes_vote(interaction.author)
+        self.stop()
 
     @disnake.ui.button(label="Not a dog", style=disnake.ButtonStyle.blurple)
-    async def no(self, _: disnake.ui.Button, interaction: disnake.MessageInteraction):
-        self.clear_votes_for_author(interaction.author)
-        self.dog_act.no_votes.append(interaction.author)
+    async def no(self, _: disnake.ui.Button, interaction: disnake.MessageInteraction) -> None:
+        """
+        Generates the 'no' option. Clicking this button causes the clicker to be added to the 'no' votes list.
 
-        await self.finish_if_complete()
+        :param _: Unused parameter for the button the user can click.
+        :param interaction: Details about the interaction that occurred with the button.
+        """
+        self.dog_act.add_new_no_vote(interaction.author)
+        self.stop()
 
-    def clear_votes_for_author(self, author: Member):
-        if author in self.dog_act.yes_votes:
-            self.dog_act.yes_votes.remove(author)
-        if author in self.dog_act.no_votes:
-            self.dog_act.no_votes.remove(author)
-
-    async def finish_if_complete(self):
-        # if self.dog_act.vote_outcome() is not None:
+    def on_timeout(self) -> None:
+        """
+        Overrides what to do when the view times out.
+        On Timeout, the dog act is updated, and we cancel waiting.
+        """
+        self.dog_act.timed_out = True
         self.stop()
 
 
@@ -167,11 +224,8 @@ class Dog(commands.Cog, name="dog-slash"):
             # Don't do anything until an outcome is reached.
             await choices.wait()
 
-        if dog_act.vote_outcome():
-            embed = disnake.Embed(description=f"{dog_act.target.mention} has been found guilty of being a dog!")
-        else:
-            embed = disnake.Embed(description=f"{dog_act.target.mention} is innocent! "
-                                              f"Shame on {dog_act.reporter.mention}")
+        embed = disnake.Embed(description=dog_act.create_outcome_message())
+        print(dog_act.create_detailed_outcome_message())
 
         await message.edit(embed=embed, view=None)
 
