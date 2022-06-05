2d16cea06d0f12d2601b20e529f06f9aaca70df3
James McDowell <wejrox@users.noreply.github.com>
2022-06-05 21:08:47 +1000

    Use a sqlite database with Peewee ORM to store records.
    Had to make a repeating string field entry since one wasn't already available. Opted for this method instead of normalising because there was no reason to add the complexity.
    DogAct no longer stores any non-primitive values, so it now does lookups to get the members when necessary. Slightly slower but not noticeable.
    Database is stored in a place that can be made a mounted volume for deployment persistence.

--- cogs/normal/dog.py
+++ cogs/normal/dog.py
@@ -4,37 +4,111 @@ import disnake
 from disnake import Member
 from disnake.ext import commands
 from disnake.ext.commands import Context
+from peewee import SqliteDatabase, Model, IntegerField, CharField, BooleanField, AutoField, Field
+
+# Store a reference to the database used by this bot.
+db = SqliteDatabase("database/dog_history.db")
 
 
-class DogAct:
+class RepeatingIntegerField(Field):
+    """
+    A simple repeating integer field, which stores the data as a comma separated string on insertion.
+    """
+    field_type = "REPEATING_INT"
+
+    def db_value(self, value: list[int]) -> str:
+        """
+        Transforms the incoming list of integers into a comma separated string for storage.
+        This is automatically applied when changes are saved to the database.
+
+        :param value: Integers to transform.
+        :return: A comma separated string representing the integers provided.
+        """
+        return ",".join(map(str, value))
+
+    def python_value(self, value: Union[str, int]) -> list[int]:
+        """
+        Transforms the retrieved comma separated string into a list of integers for use within code.
+        This is automatically applied when values are retrieved from the database.
+
+        :param value: A comma separated string representing the integer list desired.
+        :return: The integers within the string as a list, delimited by commas.
+        """
+        if not value:
+            return []
+
+        # Upon retrieval from the database, if there's only one value in the list e.g. "1234",
+        # it is automatically changed into an int by Python magic. In that case, just return it since it's done what we
+        # want already.
+        if type(value) is int:
+            return [value]
+
+        split_values = value.split(",")
+        return list(map(lambda str_value: int(str_value), split_values))
+
+
+class BaseModel(Model):
+    class Meta:
+        """
+        Tell the model to use the db defined at the top of the file.
+        """
+        database = db
+
+
+class DogAct(BaseModel):
     """
     Represents an instance in which users cast votes to determine whether a specific act should be considered dog.
-
-    :ivar yes_votes: Unique member identifiers for those that voted yes.
-    :ivar no_votes: Unique member identifiers for those that voted no.
-    :ivar timed_out: Whether this dog act sat idle for too long and has been cancelled.
     """
+    act_id = AutoField()
+    message_id: int = IntegerField(null=True, help_text="Unique identifier for the bot message that this relates to.")
+    reporter: int = IntegerField(help_text="Who it was that filed this report against the target.")
+    target: int = IntegerField(help_text="The user accused of being a dog.")
+    allegation: str = CharField(
+        help_text="The reason provided by the reporter as to why they should be considered a dog.")
+    required_votes: int = IntegerField(
+        help_text="How many votes are required in order for this dog act to be finalised for either outcome.")
+    yes_votes: list[int] = RepeatingIntegerField(default=[],
+                                                 help_text="Unique member identifiers for those that voted yes, comma separated.")
+    no_votes: list[int] = RepeatingIntegerField(default=[],
+                                                help_text="Unique member identifiers for those that voted no, comma separated.")
+    timed_out: bool = BooleanField(default=False,
+                                   help_text="Whether this dog act sat idle for too long and has been cancelled.")
 
-    def __init__(self, reporter: Member, target: Member, allegation: str,
-                 message_id: int = -1, required_votes: int = 1):
+    def set_message_id(self, message_id: int) -> None:
         """
-        :param reporter: Who it was that filed this report against the target.
-        :param target: The user accused of being a dog.
-        :param allegation: The reason provided by the reporter as to why they should be considered a dog.
-        :param message_id: Unique identifier for the bot message that this relates to.
-        Can't exist before a message has been sent by the bot to the server.
-        :param required_votes: How many votes are required in order for this DogAct to be finalised in either direction.
+        Updates the message id that is attached to this dog act.
+
+        :param message_id: Unique id of the Discord message that is attached to this dog act.
         """
         self.message_id = message_id
-        self.reporter = reporter
-        self.target = target
-        self.allegation = allegation
-        self.required_votes = required_votes
+        self.save()
 
-        # Default values for mutable variables.
-        self.yes_votes: list[Member] = []
-        self.no_votes: list[Member] = []
-        self.timed_out = False
+    def add_new_yes_vote(self, author: Member) -> None:
+        """
+        Updates the vote for the provided author to be a 'yes' vote.
+
+        :param author: Discord Member to be added to the 'yes' list.
+        """
+        self._clear_votes_for_author(author.id)
+        self.yes_votes.append(author.id)
+        self.save()
+
+    def add_new_no_vote(self, author: Member) -> None:
+        """
+        Updates the vote for the provided author to be a 'no' vote.
+
+        :param author: Discord Member to be added to the 'no' list.
+        """
+        self._clear_votes_for_author(author.id)
+        self.no_votes.append(author.id)
+        self.save()
+
+    def time_out(self) -> None:
+        """
+        Times out the current dog act.
+        """
+        self.timed_out = True
+        self.save()
 
     def vote_outcome(self) -> Union[bool, None]:
         """
@@ -49,75 +123,65 @@ class DogAct:
         else:
             return None
 
-    def add_new_yes_vote(self, author: Member) -> None:
-        """
-        Updates the vote for the provided author to be a 'yes' vote.
-
-        :param author: Discord Member to be added to the 'yes' list.
-        """
-        self._clear_votes_for_author(author)
-        self.yes_votes.append(author)
-
-    def add_new_no_vote(self, author: Member) -> None:
-        """
-        Updates the vote for the provided author to be a 'no' vote.
-
-        :param author: Discord Member to be added to the 'no' list.
-        """
-        self._clear_votes_for_author(author)
-        self.no_votes.append(author)
-
-    def create_updated_dog_act_message(self) -> str:
+    async def create_updated_dog_act_message(self, context: Context) -> str:
         """
         Generates a message detailing information about this dog acct report, including the current status of voting.
 
         :returns: An up-to-date representation of the status of this dog act.
         """
-        return (f"Whoah there {self.reporter.mention}, that's a big claim!\n"
-                f"Who agrees that {self.target.mention} was really a dog for '{self.allegation}'?\n"
-                f"Required votes: {self.required_votes}\n"
+        reporter_member = await context.guild.get_or_fetch_member(self.reporter)
+        target_member = await context.guild.get_or_fetch_member(self.target)
+        return (f"Whoah there {reporter_member.mention}, that's a big claim!\n"
+                f"Who agrees that {target_member.mention} was really a dog for '{self.allegation}'?\n"
+                f"Votes required on one side for a verdict: {self.required_votes}\n"
                 f"Current votes: Guilty - {len(self.yes_votes)}, Not Guilty - {len(self.no_votes)}")
 
-    def create_outcome_message(self) -> str:
+    async def create_outcome_message(self, context: Context) -> str:
         """
         Generates a message indicating to the reader what the outcome was of the dog act trial.
 
         :return: Whether the target is guilty or innocent.
         """
+        reporter_member = await context.guild.get_or_fetch_member(self.reporter)
+        target_member = await context.guild.get_or_fetch_member(self.target)
         if self.vote_outcome():
-            return f"{self.target.mention} has been found guilty of being a dog for '{self.allegation}'!"
+            return f"{target_member.mention} has been found guilty of being a dog for '{self.allegation}'!"
         elif self.timed_out:
-            return f"{self.target.mention} has been found innocent due to lack of voter participation!"
+            return f"{target_member.mention} has been found innocent due to lack of voter participation!"
         else:
-            return f"{self.target.mention} has been found innocent! Shame on {self.reporter.mention}"
+            return f"{target_member.mention} has been found innocent! Shame on {reporter_member.mention}"
 
-    def create_detailed_outcome_message(self) -> str:
+    async def create_detailed_outcome_message(self, context: Context) -> str:
         """
         Creates a detailed outcome message containing information about the trial.
         Shouldn't be sent to the server as it's not in a nice to read format.
 
         :returns: Information about the dog act.
         """
+        yes_voters = await context.guild.get_or_fetch_members(self.yes_votes)
+        no_voters = await context.guild.get_or_fetch_members(self.no_votes)
 
-        def get_voter_name(user: Member): return user.name
+        def get_voter_name(user: Member):
+            return user.name
 
-        guilty_voters: list[str] = list(map(get_voter_name, self.yes_votes))
-        not_guilty_voters: list[str] = list(map(get_voter_name, self.no_votes))
+        guilty_voters: list[str] = list(map(get_voter_name, yes_voters))
+        not_guilty_voters: list[str] = list(map(get_voter_name, no_voters))
 
         return (f"Dog act finalised. "
-                f"Verdict: {self.create_outcome_message()}. "
+                f"Verdict: {await self.create_outcome_message(context)}. "
                 f"Guilty voters: {guilty_voters}, Not guilty voters: {not_guilty_voters}")
 
-    def _clear_votes_for_author(self, author: Member) -> None:
+    def _clear_votes_for_author(self, author: int) -> None:
         """
         Removes all votes previously made by the provided author from any relevant lists.
+        Doesn't update the database.
 
         :param author: Discord Member to be removed.
         """
         if author in self.yes_votes:
             self.yes_votes.remove(author)
-        if author in self.no_votes:
-            self.no_votes.remove(author)
+        if author in self.yes_votes:
+            self.yes_votes.remove(author)
 
 
 class DogChoice(disnake.ui.View):
@@ -170,7 +234,7 @@ class DogChoice(disnake.ui.View):
         Overrides what to do when the view times out.
         On Timeout, the dog act is updated, and we cancel waiting.
         """
-        self.dog_act.timed_out = True
+        self.dog_act.time_out()
         self.stop()
 
 
@@ -182,9 +246,11 @@ class Dog(commands.Cog, name="dog"):
     a mark added against their name.
     """
     votes_to_complete: int = 2
-    dog_acts: list[DogAct] = []
 
     def __init__(self, bot):
+        db.connect()
+        db.create_tables([DogAct])
+        db.close()
         self.bot = bot
 
     # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
@@ -208,15 +274,14 @@ class Dog(commands.Cog, name="dog"):
         member = await context.guild.get_or_fetch_member(target.id)
 
         # Initialise the dog act, recording details about the message.
-        dog_act = DogAct(reporter=context.author, target=member, allegation=reason,
-                         required_votes=self.votes_to_complete)
-        self.dog_acts.append(dog_act)
+        dog_act = DogAct.create(reporter=context.author.id, target=member.id, allegation=reason,
+                                required_votes=self.votes_to_complete)
 
         # Continue waiting for user input until an outcome has been reached.
         message = None
         while dog_act.vote_outcome() is None:
             # Set the embed to the current status of the trial.
-            embed = disnake.Embed(description=dog_act.create_updated_dog_act_message(),
+            embed = disnake.Embed(description=await dog_act.create_updated_dog_act_message(context),
                                   colour=0x9C84EF)
             choices = DogChoice(dog_act)
 
@@ -225,18 +290,18 @@ class Dog(commands.Cog, name="dog"):
             if message is None:
                 message = await context.send(embed=embed, view=choices)
 
-                # Assign the newly created message to our records for future reference.
-                dog_act.message_id = message.id
+                # Assign the newly created message to our record for future reference.
+                dog_act.set_message_id(message.id)
             else:
                 await message.edit(embed=embed, view=choices)
 
             # Don't do anything until another interaction occurs.
             await choices.wait()
 
-        embed = disnake.Embed(description=dog_act.create_outcome_message())
+        embed = disnake.Embed(description=await dog_act.create_outcome_message(context))
         await message.edit(embed=embed, view=None)
 
-        print(dog_act.create_detailed_outcome_message())
+        print(await dog_act.create_detailed_outcome_message(context))
 
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
